c8abbd775d6fd542434e8857cacda542cb87557d
Balaji <93272649+codingisf@users.noreply.github.com>
2026-03-06 21:41:47 +0530

    Log DB_CONNECTION during database pool creation
    Added logging for DB_CONNECTION environment variable.

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 const mysql = require('mysql2/promise');
-require("dotenv").config();
 
+console.log("DB_CONNECTION:", process.env.DB_CONNECTION);
 const pool = mysql.createPool(process.env.DB_CONNECTION);
+
 
 
 module.exports = pool;
